a569e446988344473f4e545b19628a7231996674
dave <dave@tomorrowready.com>
2025-08-26 22:23:27 +0000

    Firebase hosting update

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC1ntFGnFAIA_v6MzILHDAqr1_IogczME4",
+    authDomain: "tm38-calc.firebaseapp.com",
+    projectId: "tm38-calc",
+    storageBucket: "tm38-calc.firebasestorage.app",
+    messagingSenderId: "216948323726",
+    appId: "1:216948323726:web:f77d8d09eba27b778e20df",
+    measurementId: "G-4V8E1YSHQT"
