bcf9573fd54fbc8486f43a929ad4cdf5c9efc494
fall3nNZ <71684233+fall3nNZ@users.noreply.github.com>
2025-08-28 06:00:18 +1200

    Update

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC1ntFGnFAIA_v6MzILHDAqr1_IogczME4",
-  authDomain: "tm38-calc.firebaseapp.com",
-  projectId: "tm38-calc",
-  storageBucket: "tm38-calc.firebasestorage.app",
-  messagingSenderId: "216948323726",
-  appId: "1:216948323726:web:3276772e63e709ea8e20df",
-  measurementId: "G-8KCQYYP3J8"
+  
 };
 
 // Initialize Firebase
